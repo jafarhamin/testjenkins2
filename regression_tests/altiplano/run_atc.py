7cb24d577a29c8a1f60e4d4a7b75d77f3eee1da8
Jafar Hamin <jafarhmn@yahoo.com>
2021-07-12 16:58:37 +0200

    Update run_atc.py

--- regression_tests/altiplano/run_atc.py
+++ regression_tests/altiplano/run_atc.py
@@ -88,6 +88,7 @@ def launch_test_parameters():
 
 
 def launch_test_batch():
+    sutil.run('source {}/atc/env/.profile.common; source {}/robot/.robot.profile'.format(HOST_REPO_PATH, HOST_REOP_PATH))
     script_path = '{}/atc/cm8/auto/tools/pbscript/launchTestBatch'.format(HOST_REPO_PATH)
     sutil.run('chmod +x {}'.format(script_path))
     parameters = launch_test_parameters()
